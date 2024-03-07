26231479c004955070494ff3ef605d6ae3af554f
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-08 01:46:42 +0300

    Add NoteDetail page to router configuration

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import { NewNote } from "../pages/NewNote";
 import NoteList from "../pages/NoteList";
+import NoteDetail from "../pages/NoteDetail";
 
 export const router = createBrowserRouter([
   {
@@ -13,7 +14,7 @@ export const router = createBrowserRouter([
         path: "/new",
         element: <NewNote />,
       },
-      { path: "/detail/:id", element: <div>Post Details</div> },
+      { path: "/detail/:id", element: <NoteDetail /> },
       { path: "/detail/:id/edit", element: <div>Post edit</div> },
 
       { path: "*", element: <h1>Not Found</h1> },
