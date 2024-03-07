63f4e09d5bfe7975491f6b6469410b3308c5e4df
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-07 13:00:13 +0300

    Add useLocalStorage custom hook for handling localStorage state

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, initalValue: T | (() => T)) {
+  const [value, setValue] = useState<T>(() => {
+    const jsonValue = localStorage.getItem(key);
+
+    if (jsonValue == null) {
+      if (typeof initalValue === "function") {
+        return (initalValue as () => T)();
+      } else {
+        return initalValue;
+      }
+    } else {
+      return JSON.parse(jsonValue);
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as [T, typeof setValue];
+}
