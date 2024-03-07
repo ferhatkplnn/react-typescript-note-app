205b1c03808ce9c9016f2241a94365aac48fabcd
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-07 05:03:44 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bgNavy: "#160040",
+        primary: "#9A0680",
+        secondary: "#79018C",
+        lightNavy: "#4C0070",
+      },
+      fontFamily: {
+        kalam: ["Kalam", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
