72f48f6f947d1e2a954d76dc0755b5dd570b8481
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-08 01:02:45 +0300

    Add Link to NoteDetail page in NoteListCard component

--- src/components/NoteListCard.tsx
+++ src/components/NoteListCard.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
 import { NoteWithTags } from "../types/types";
 import Tag from "./Tag";
 
 const NoteListCard = ({ note }: { note: NoteWithTags }) => {
   return (
-    <div className="border-4 flex flex-col justify-center items-center text-center min-h-60 break-all p-4 border-primary rounded hover:scale-105 duration-300 cursor-pointer">
-      <h3>CSS:has Selecto</h3>
+    <Link
+      to={`/detail/${note.id}`}
+      className="border-4 flex flex-col justify-center items-center text-center min-h-60 break-all p-4 border-primary rounded hover:scale-105 duration-300 cursor-pointer"
+    >
+      <h3>{note.title}</h3>
       <div className="space-x-2">
         {note.tags.map((tag, index) => (
           <Tag variantNum={index} key={tag.id}>
@@ -12,7 +16,7 @@ const NoteListCard = ({ note }: { note: NoteWithTags }) => {
           </Tag>
         ))}
       </div>
-    </div>
+    </Link>
   );
 };
 
