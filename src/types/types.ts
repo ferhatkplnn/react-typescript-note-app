80f50e3012356f658da71cdd4a31a90c0c197a24
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-07 18:34:29 +0300

    Combine Note and RawNote types into NoteWithTags type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,24 +1,26 @@
+export type Tag = {
+  id: string;
+  label: string;
+};
+
 export type NoteData = {
   title: string;
   markdown: string;
   tags: Tag[];
 };
 
-export type Tag = {
-  id: string;
-  label: string;
-};
-
 export type Note = {
   id: string;
 } & NoteData;
-
-export type RawNote = {
-  id: string;
-};
 
 export type RawNoteData = {
   title: string;
   markdown: string;
   tagIds: string[];
 };
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type NoteWithTags = RawNote & { tags: Tag[] };
