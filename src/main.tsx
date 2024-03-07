6803d6303148b828f4644f258b057091f83137c2
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-07 13:25:17 +0300

    Add NoteProvider to manage notes and tags state in the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.tsx";
+import { NoteProvider } from "./context/NoteContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <NoteProvider>
+      <RouterProvider router={router} />
+    </NoteProvider>
   </React.StrictMode>
 );
